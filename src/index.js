efd47f2372120e6c759ace399fbc33607409ea6b
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2023-09-30 16:28:35 +0300

    Init handlebars + show first view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+const PORT = 5050;
+
+// Handlebars config
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.get("/", (req, res) => {
+    res.render("layouts/main");
+});
+
+app.listen(PORT, () => {console.log(`Server is working on port ${PORT}!`)});
