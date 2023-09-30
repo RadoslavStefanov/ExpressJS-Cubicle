fcc20070eca09e5789769e5bc13ae04ce60fced1
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2023-09-30 17:00:39 +0300

    Removed uneeded variable + index view fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 5050;
@@ -9,8 +10,11 @@ app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+//Setup static files
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.render("layouts/main");
+    res.render("index");
 });
 
 app.listen(PORT, () => {console.log(`Server is working on port ${PORT}!`)});
