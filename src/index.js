1b734864d716471f77870ddebec5ad4dae8d173d
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2023-10-01 00:49:23 +0300

    Added basic routing

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
+const routes = require("./router");
 
 const app = express();
 const PORT = 5050;
 
-
+// Configs
 expressConfig(app, express);
 handlebarsConfig(app, handlebars);
 
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
+//Routes
+app.use(routes)
 
 app.listen(PORT, () => {console.log(`Server is working on port ${PORT}!`)});
