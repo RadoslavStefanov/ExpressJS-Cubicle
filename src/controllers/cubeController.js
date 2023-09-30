1a89789834761c12aa45a15c71788e0ead1d1f98
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2023-10-01 01:18:54 +0300

    Added cube creating logic

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
+const cubeSevice = require("../services/cubeService");
 
 router.get( "/create", (req, res) => {
+    console.log(cubeSevice.getAll());
     res.render("create");
 });
 
+
+router.post( "/create", (req, res) => {
+    const { name, description, imageURL, difficultyLevel } = req.body;
+
+    cubeSevice.create({
+        name,
+        description,
+        imageURL,
+        difficultyLevel: Number(difficultyLevel)
+    });
+
+    res.redirect("/");
+});
 
 module.exports = router;
---
+++ src/services/cubeService.js
@@ -0,0 +1,18 @@
+const uniqid = require("uniqid");
+const cubes = [];
+
+exports.create = (cubeData) => {
+
+    const newCube = {
+        id: uniqid(),
+        ...cubeData
+    }
+    
+    cubes.push(newCube);
+
+    return newCube;
+}
+
+exports.getAll = () => {
+    return [...cubes];
+}
